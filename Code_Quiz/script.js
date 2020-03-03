edf053e814a85c68ea38ecaa7c609f7911c3de17
rpounceyjr <rpounceyjr@gmail.com>
2020-03-03 14:53:00 -0500

    Solved how to access key from an object as question.  Need to solve problem of assigning eventlistener to li.

--- Code_Quiz/script.js
+++ Code_Quiz/script.js
@@ -2,6 +2,8 @@ var timerEl = document.querySelector("#timer");
 var secondsLeft = 5
 var quizBeginEl = document.querySelector("#quiz-begin");
 var quizEl = document.querySelector("#quiz");
+var li;
+
 
 quizBeginEl.textContent = "Click Here to Begin";
 //TIMER
@@ -13,47 +15,63 @@ function timer() {
 }
 
 function countDown() {
-    if(secondsLeft < 0){
+    if (secondsLeft < 0) {
         stopTimer()
-    }else{
-    timerEl.textContent = secondsLeft;
-    secondsLeft--;
-}
+        // timerEl.textContent = "Time's Up!";
+    } else {
+        timerEl.textContent = secondsLeft;
+        secondsLeft--;
+    }
 }
 
 function stopTimer() {
     clearInterval(timer);
 }
-   
+
 
 
 
 //QUIZ
+//find out how to assign true/false values to answers
+//randomize choice order?
 //when a question is answered correctly a message is displayed 
 //when question is answered incorrectly a message is displayed
 //if a question is incorrectly answered, time is deducted from timer
 //after question is answered and message is displayed
-   //question and answer choices are cleared and new question is populated
+//question and answer choices are cleared and new question is populated
 //when all questions are answered game is over
 
-var firstQuestion = "Here's the first question:";
-var firstQuestionChoicesArray = {"first choice": true,"second choice":false, "third choice":false, "fourth choice":false};
+var firstQuestion = "Here's the first question";
+var firstQuestionAnswersObject = { "first choice": true, "second choice": false, "third choice": false, "fourth choice": false };
+// var firstQuestionChoicesArray = ["first choice", "second choice", "third choice", "fourth choice"];
 
-quizBeginEl.addEventListener("click", function(){
+quizBeginEl.addEventListener("click", function () {
     quizBeginEl.textContent = "";
 
     timer();
-    questionAndAnswers(firstQuestion,firstQuestionChoicesArray);
+    questionAndAnswers(firstQuestion, firstQuestionAnswersObject);
 })
 
-function questionAndAnswers(question, choicesArray) {
+function questionAndAnswers(question, choicesObject) {
     quizEl.textContent = question;
-    for (var i = 0; i < choicesArray.length; i++) {
+    for (var key in choicesObject) {
         var li = document.createElement("li");
-        li.textContent = choicesArray[i];
+        li.textContent = key;
         quizEl.appendChild(li);
-       }
+    }
 }
+
+// li.addEventListener("click", function() {
+//     if (Object.keys(li) === true) {
+//         var message = document.createElement("div");
+//         message.textContent = "Correct!"
+//         quizEl.appendChild(message);
+//     }
+// })
+
+
+
+
 
 
 //SAVE INITIALS AND SCORE
